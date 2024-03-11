a2eafbd6c10828c1f393882d822b1ff1e3e18bac
Trapti Singh <128883459+trapti09@users.noreply.github.com>
2024-03-11 19:12:52 +0530

    Add files via upload

---
+++ trapti/trapti/forget.php
@@ -0,0 +1,22 @@
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="assets/CSS/about.css" />
+    </head>
+    <body>
+        <div class="box"></div>
+           <div class="inner-box">
+            <h3><center>Enter your Email Address.</center></h3>
+            <center><input type="email" placeholder="Email Address" /></center>
+            <p style="text-align: center;">
+            <br>
+            <br>
+            <BUTTON TYPE="button" ONCLICK="alert('Link Sent to your email.')">Send Recovery link!</BUTTON>
+            <br>
+            <br>
+            <a class="link" href="register.php"><h6>Back to Login Page</h6></a>
+        </p>
+           </div>
+    </body>
+</html>
